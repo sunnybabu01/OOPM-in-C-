e7132c93a6d45fd1245195cf8bba1fea35cc57b4
Sunny Ranjan <sunnyranjan0448@gmail.com>
2025-03-07 20:38:10 +0530

    Add files via upload

---
+++ linked_sinlist.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+struct slist{
+    int data;
+    struct slist*next;
+    
+};
+typedef struct slist node;
+node*head=NULL;
+void createLinkedList(){
+    node*newNode;
+    int data; 
+
+    cout<<"Enter the data for the new node : "<<endl;
+    cin>>"%d", &data>>endl;
+
+
+    newNode 
+}
+
+
+
+
+
+int main(){
+
+    return ;
+}
